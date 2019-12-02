fba3a454c32705b773263b2d4caff0cf7668c101
Fernando <fernandosouzaconceicao@gmail.com>
2019-12-02 20:19:24 -0300

    002
    nome do usuario do git invalido

--- src/main/java/model/Constantes.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public final class Constantes {
-
-	public final static String[] listaServicos = {"iFood", "Uber", "Spotify", "NetFlix"};
-	
-	public final static String[] listaGeneros= {"Masculino", "Feminino"};
-}
